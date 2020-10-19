51438a3d37978881dfc6e9e0c365deb888ffaff1
Luke Hollenback <8144622+lukehollenback@users.noreply.github.com>
2020-10-18 21:28:35 -0600

    Added ability to take screenshots.

--- objects/interface/Interface.gd
+++ objects/interface/Interface.gd
@@ -48,6 +48,14 @@ func _input(event):
 			create_dialog("journal")
 		else:
 			create_dialog("tickled")
+	elif event.is_action_pressed("screenshot"):
+		var capture = get_viewport().get_texture().get_data()
+		
+		capture.flip_y()
+		capture.resize(capture.get_width() * 4, capture.get_height() * 4, false)
+		capture.save_png("user://screenshot.png")
+		
+		print("Screenshot taken and saved to %s." % (OS.get_user_data_dir() + "/screenshot.png"))
 #
 # Returns whether or not a modal is currently displayed and all other actions should thus be
 # prevented.
